93341218ac87bc29e95d2bcce531201e86371daa
jonathancarvalheiro <57483006+jonathancarvalheiro@users.noreply.github.com>
2021-03-15 08:10:49 +0000

    Create dummy.js

---
+++ dummy.js
@@ -0,0 +1,8 @@
+/* global QamineUtils */\n" +
+            "\n" +
+            "\"use strict\";\n" +
+            "\n" +
+            "(function($, window) {\n" +
+            "  $('.jsSelectedMainEmail').text(postVariable.email);\n" +
+            "  return 0;\n" +
+            "})(jQuery, window);"
